25476d2cdb0c5a19b7e35051dd465a3e57b6f357
fina <finafitria1395@gmail.com>
2024-03-12 13:37:56 +0700

    finish to do assessmnet

---
+++ src/StringArray.java
@@ -0,0 +1,11 @@
+public class StringArray {
+    public static void main(String[] args) {
+        String[]anagram = {"cook", "save", "taste", "aves", "vase", "state", "map"};
+
+        System.out.println(anagram[0]);
+        System.out.println(anagram[1] + "," + anagram[3] + "," + anagram[4]);
+        System.out.println(anagram[2] + "," + anagram[5]);
+        System.out.print(anagram[6]);
+
+    }
+}
